b944339b574829e5c2c2556f15cf5efd2e7e1845
chichi-ugo <cnugochukwu@email.wm.edu>
2022-07-15 07:44:22 -0400

    Updating images and queries.sql

--- queries.sql
+++ queries.sql
@@ -204,7 +204,11 @@ GROUP BY ed.dept_name
 ORDER BY COUNT(ed.emp_no)DESC;
 
 -- Retiring employees by department
-SELECT DISTINCT ON (de.emp_no) d.dept_name, de.emp_no, de.dept_no, e.first_name, e.last_name
+SELECT DISTINCT ON (de.emp_no) d.dept_name, 
+    de.emp_no, 
+    de.dept_no, 
+    e.first_name, 
+    e.last_name
 INTO retire_dept
 FROM departments AS d
 INNER JOIN dept_emp AS de
@@ -212,7 +216,7 @@ ON (d.dept_no = de.dept_no)
 INNER JOIN employees AS e
 ON (de.emp_no = e.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-    AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+    AND (de.to_date = '9999-01-01')
 ORDER BY de.emp_no;
 
 
